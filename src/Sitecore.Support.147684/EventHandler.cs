5daf2e9204cb9ea210f6745d3c53dd99b73273d6
Alen Pelin <alenpelin@gmail.com>
2018-04-20 10:52:04 +1000

    Reset branch ID to work around language fallback

--- src/Sitecore.Support.147684/EventHandler.cs
+++ src/Sitecore.Support.147684/EventHandler.cs
@@ -2,9 +2,11 @@
 {
   using System;
 
+  using Sitecore.Data;
   using Sitecore.Data.Items;
   using Sitecore.Diagnostics;
   using Sitecore.Events;
+  using Sitecore.SecurityModel;
 
   public class EventHandler
   {
@@ -13,6 +15,19 @@
     {
       var item = Event.ExtractParameter(args, 0) as Item;
       Assert.IsNotNull(item, nameof(item));
+
+      if (item.BranchId == ID.Null)
+      {
+        return;
+      }
+
+      using (new SecurityDisabler())
+      {
+        using (new EditContext(item, false, false))
+        {
+          item.BranchId = ID.Null;
+        }
+      }
     }
   }
 }
