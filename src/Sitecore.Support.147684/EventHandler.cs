0886937cce94794a334c02e99e2f48e021cdc9ca
Alen Pelin <alenpelin@gmail.com>
2018-04-20 10:51:20 +1000

    Add item:added handler to inject fix with

---
+++ src/Sitecore.Support.147684/EventHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Sitecore.Support
+{
+  using System;
+
+  using Sitecore.Data.Items;
+  using Sitecore.Diagnostics;
+  using Sitecore.Events;
+
+  public class EventHandler
+  {
+    [UsedImplicitly]
+    public void OnItemAdded(object sender, EventArgs args)
+    {
+      var item = Event.ExtractParameter(args, 0) as Item;
+      Assert.IsNotNull(item, nameof(item));
+    }
+  }
+}
